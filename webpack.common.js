761e5797100fac31bf754cf100fac78773f00402
svereschako <svereschako@gmail.com>
2017-11-23 19:30:54 +0300

    first commit

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+  module.exports = {
+    entry: './src/react.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+   module: {
+     rules: [
+    {
+         test: /\.css$/,
+         use: [
+           'style-loader',
+           'css-loader'
+         ]
+    },
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+        //  presets: ['@babel/preset-env']
+        }
+      }
+    }
+    ]
+   }
+};
